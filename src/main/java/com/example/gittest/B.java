a1d8d41c3b418beaac9f22c3ae7e6a5493afdd9a
bawool <bawool1010@gmail.com>
2024-02-21 16:06:33 +0900

    commit 40c5add0ddb537a1a176515df0b7619e46ca0151 Author: bawool <bawool1010@gmail.com> Date:   Wed Feb 21 16:05:34 2024 +0900
        line delete B
    
    commit 8bf8fb74e0339e79360dd6faf1443f07461a83c1
    Author: bawool <bawool1010@gmail.com>
    Date:   Wed Feb 21 16:05:15 2024 +0900
    
        line delete A
    
    commit a0c4751d6884c3f2aae52133d998fc5342a306db
    Author: bawool <bawool1010@gmail.com>
    Date:   Wed Feb 21 16:01:38 2024 +0900
    
        ccc
    
    commit 092e900ad5b8ba0fff4b30744ccb537107ee5970
    Author: bawool <bawool1010@gmail.com>
    Date:   Wed Feb 21 16:01:10 2024 +0900
    
        bbb
    
    commit 94e99e466d59677429f6b198865a98cdf2df765e
    Author: bawool <bawool1010@gmail.com>
    Date:   Wed Feb 21 16:00:21 2024 +0900
    
        aaa
    a, b

--- src/main/java/com/example/gittest/B.java
+++ src/main/java/com/example/gittest/B.java
@@ -1,4 +1,6 @@
 package com.example.gittest;
 
 public class B {
+    public static void main(String[] args) {
+    }
 }
--- src/main/java/com/example/gittest/C.java
+++ src/main/java/com/example/gittest/C.java
@@ -2,5 +2,10 @@ package com.example.gittest;
 
 public class C {
     public void test() {
+
+
+
+
+
     }
 }
