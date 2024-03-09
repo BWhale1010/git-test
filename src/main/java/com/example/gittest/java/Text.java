7a06de3f115bc2bfb6f20b7e52c85e93eb9e60f9
bawool <bawool1010@gmail.com>
2024-03-09 20:05:29 +0900

    수정

---
+++ src/main/java/com/example/gittest/java/Text.java
@@ -0,0 +1,22 @@
+package com.example.gittest.java;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class Text {
+    private int size;
+    private String description;
+    private List<String> list;
+    private Map<String,Integer> map;
+
+    public void test() {
+        list.add("A");
+        list.add("B");
+
+        list.remove("A");
+
+    }
+
+
+}
---
+++ src/main/java/com/example/gittest/java/TypeClass.java
@@ -0,0 +1,12 @@
+package com.example.gittest.java;
+
+public class TypeClass {
+    Object object = new Object();
+    String studentName = "steve"; // 학생이름
+
+    /*
+    * 블록
+    * 주석
+    * */
+
+}
